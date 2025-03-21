94cee871670b38c91046b0f293666ecc941720c5
Pavel Krasovsky <frostcat2001@gmail.com>
2025-03-21 09:58:38 +0300

    add: create user mutation

--- src/modules/auth/account/account.service.ts
+++ src/modules/auth/account/account.service.ts
@@ -1,5 +1,7 @@
 import { PrismaService } from '@/src/core/prisma/prisma.service';
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { CreateUserInput } from './inputs/create-user.input';
+import { hash } from 'argon2';
 
 @Injectable()
 export class AccountService {
@@ -9,5 +11,40 @@ export class AccountService {
         const users = await this.prismaService.user.findMany();
         
         return users;
+    }
+
+    public async create(input: CreateUserInput){
+        const {username, email, password} = input
+
+        const isUserNameExists = await this.prismaService.user.findUnique({
+            where: {
+                username
+            }
+        })
+
+        if(isUserNameExists) {
+            throw new ConflictException('Username already exists')
+        }
+        
+        const isUserEmailExists = await this.prismaService.user.findUnique({
+            where: {
+                email
+            }
+        })
+
+        if(isUserEmailExists) {
+            throw new ConflictException('Email already exists')
+        }
+
+       await this.prismaService.user.create({
+            data: {
+                username,
+                email,
+                password: await hash(password),
+                displayName: username
+            }
+        })
+
+        return true
     }
 }
